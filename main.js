789c43fc80ca5f705825755a41c28cdcecc79d9e
viniciusrs <viniciusrubensdasilva@gmail.com>
2017-10-16 19:42:55 -0200

    Changed the content type to json application

--- main.js
+++ main.js
@@ -9,7 +9,13 @@ const app = express();
 const user = require('./routes/users');
 const character = require('./routes/characters');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //GET
 app.get('/user', user.get);
